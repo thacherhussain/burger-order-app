92f87952bb1f87510bf5a59b554f4d10d81ed397
Thacher <thacher@tidepool.org>
2020-08-14 13:17:15 -0700

    added contact data form to checkout page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,31 +1,62 @@
 import React from "react";
-import { Button, Row, Col, Container } from "react-bootstrap";
-import { Link } from 'react-router-dom';
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 
 const Checkout = () => {
 	return (
 		<>
-		<Helmet>
-			<title>Checkout | Good Burger</title>
-		</Helmet>
-		<Container fluid>
-			<Row>
-				<Col></Col>
-				<Col md={10} sm={10} className="justify-content-center">
-					<h1>Checkout</h1>
-					<p>Order #</p>
-					<p>Order</p>
-					<p>Price</p>
-					<p>User Name</p>
-					<p>User Address</p>
-				</Col>
-				<Col></Col>
-			</Row>
-			<Row className="justify-content-center">
-				<Button size="lg" as={Link} to="/burger-builder">Confirm</Button>
-			</Row>
-		</Container>
+			<Helmet>
+				<title>Checkout | Good Burger</title>
+			</Helmet>
+			<Container fluid>
+				<Row>
+					<Col></Col>
+					<Col md={10} sm={10} className="justify-content-center">
+						<h1>Checkout</h1>
+						<p>Order #</p>
+						<p>Order</p>
+						<p>Price</p>
+						<p>User Name</p>
+						<p>User Address</p>
+					</Col>
+					<Col lg={{ offset: 2 }}>
+						<h3>Enter Your Information</h3>
+						<Form>
+							<Form.Group controlId="formBasicName">
+								<Form.Label>Name</Form.Label>
+								<Form.Control type="text" placeholder="Hermione Granger" />
+							</Form.Group>
+							<Form.Group controlId="formBasicEmail">
+								<Form.Label>Email address</Form.Label>
+								<Form.Control type="email" placeholder="ceo@spew.org" />
+							</Form.Group>
+							<Form.Group>
+								<Form.Label>Street Address</Form.Label>
+								<Form.Control type="text" placeholder="3429 Ministry Street" />
+							</Form.Group>
+							<Form.Group>
+								<Form.Label>Zipcode</Form.Label>
+								<Form.Control type="text" placeholder="87217" />
+							</Form.Group>
+							<Form.Group controlId="formDeliveryMethod">
+								<Form.Label>Delivery Method</Form.Label>
+								<Form.Control as="select">
+									<option>Standard</option>
+									<option>FedEx</option>
+									<option>Owl</option>
+									<option>Slow</option>
+								</Form.Control>
+							</Form.Group>
+						</Form>
+					</Col>
+				</Row>
+				<Row className="justify-content-center">
+					<Button size="lg" as={Link} to="/burger-builder">
+						Confirm
+					</Button>
+				</Row>
+			</Container>
 		</>
 	);
 };
