0147ba1f311975f9ba73433f17f350d34654b9c3
Thacher <thacher@tidepool.org>
2020-08-25 19:07:58 -0700

    condense to single use of Context, storeContext to globalContext to reflect that change

--- src/context/store.js
+++ src/context/store.js
@@ -2,38 +2,27 @@ import React from "react";
 import { getTotalPrice  } from 'selectors';
 
 export default function makeStore(reducer, initialState) {
-	const storeContext = React.createContext();
-	const dispatchContext = React.createContext();
+	const globalContext = React.createContext();
+	// const dispatchContext = React.createContext();
 
 	const StoreProvider = ({ children }) => {
 		const [store, dispatch] = React.useReducer(reducer, initialState);
 
 		return (
-			<dispatchContext.Provider value={dispatch}>
-				<storeContext.Provider value={store}>{children}</storeContext.Provider>
-			</dispatchContext.Provider>
-		);
-
-		return (
-			<storeContext.Provider value={{ dispatch, store }}>
+			<globalContext.Provider value={{ dispatch, store }}>
 				{children}
-			</storeContext.Provider>
-		)
+			</globalContext.Provider>
+		);
 	};
 
 	function useStore() {
-		const ctx = React.useContext(storeContext);
-		const calcTotalPrice = getTotalPrice(ctx.ingredients, ctx.prices);
-		return {...ctx, calcTotalPrice}
-
-		const { store } = React.useContext(storeContext);
-		return store;
+		const { store } = React.useContext(globalContext);
+		const calcTotalPrice = getTotalPrice(store.ingredients, store.prices);
+		return {...store, calcTotalPrice};
 	}
 
 	function useDispatch() {
-		return React.useContext(dispatchContext);
-
-		const { dispatch } = React.useContext(storeContext);
+		const { dispatch } = React.useContext(globalContext);
 		return dispatch;
 	}
 
