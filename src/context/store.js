45019b3afc4e4ba94027d2b149c81c4c4ef7cc99
Thacher Hussain <thacher.hussain@gmail.com>
2021-02-02 19:33:03 -0800

    firebase work

--- src/context/store.js
+++ src/context/store.js
@@ -1,30 +1,33 @@
-import React from "react";
-import { getTotalPrice  } from 'selectors';
+import React from 'react'
+import { getTotalPrice } from 'selectors'
 
 export default function makeStore(reducer, initialState) {
-	const globalContext = React.createContext();
-	// const dispatchContext = React.createContext();
+  const globalContext = React.createContext()
+  // const dispatchContext = React.createContext();
 
-	const StoreProvider = ({ children }) => {
-		const [store, dispatch] = React.useReducer(reducer, initialState);
+  const StoreProvider = ({ children }) => {
+    const [store, dispatch] = React.useReducer(reducer, initialState)
 
-		return (
-			<globalContext.Provider value={{ dispatch, store }}>
-				{children}
-			</globalContext.Provider>
-		);
-	};
+    return (
+      <globalContext.Provider value={{ dispatch, store }}>
+        {children}
+      </globalContext.Provider>
+    )
+  }
 
-	function useStore() {
-		const { store } = React.useContext(globalContext);
-		const calcTotalPrice = getTotalPrice(store.ingredients, store.prices);
-		return {...store, calcTotalPrice};
-	}
+  function useStore() {
+    const { store } = React.useContext(globalContext)
+    const calcTotalPrice = getTotalPrice(store.ingredients, store.prices)
+    return { ...store, calcTotalPrice }
+  }
 
-	function useDispatch() {
-		const { dispatch } = React.useContext(globalContext);
-		return dispatch;
-	}
+  function useDispatch() {
+    const { dispatch } = React.useContext(globalContext)
+    return dispatch
+  }
 
-	return [StoreProvider, useStore, useDispatch];
+  return [StoreProvider, useStore, useDispatch]
 }
+
+// const Provider1 = makeStore(reducer, {});
+// const Provider2 = makeStore(reducer, { value: 1 });
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,22 @@
 import firebase from 'firebase/app'
-import "firebase/auth"
+import 'firebase/auth'
+
+const allInitalized = [
+  'REACT_APP_API_KEY',
+  'REACT_APP_AUTH_DOMAIN',
+  'REACT_APP_DATABASE_URL',
+  'REACT_APP_PROJECT_ID',
+  'REACT_APP_STORAGE_BUCKET',
+  'REACT_APP_MESSAGING_SENDER_ID',
+  'REACT_APP_APP_ID',
+].filter((key) => !process.env[key])
+
+if (allInitalized.length > 0)
+  throw new Error(
+    `The following environment variables are missing for firebase: ${allInitalized.join(
+      ', '
+    )}`
+  )
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
