80ec5be24f09e1cf2df7b65de66b6df0ae76a21c
Thacher Hussain <thacher.hussain@gmail.com>
2021-05-13 18:39:51 -0700

    WIP

--- src/components/UI/TotalPrice.tsx
+++ src/components/UI/TotalPrice.tsx
@@ -1,12 +1,13 @@
-import React from "react"
-import { useOrders } from "context/orderContext"
-import { getTotalPrice } from "selectors"
+import React from 'react'
+import { useOrders } from 'context/orderContext'
+import { getTotalPrice } from 'selectors'
 
 const TotalPrice: React.FC = () => {
 	const {
 		store: { ingredients, prices },
 	} = useOrders()
-	const totalPrice = getTotalPrice(ingredients, prices)
+	// const totalPrice = getTotalPrice(ingredients, prices)
+	const totalPrice = 100
 
 	return <strong>Total Price: ${totalPrice.toFixed(2)}</strong>
 }
--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -1,11 +1,14 @@
-import { Ingredients, IngredientKeys } from 'common/types';
+import { Ingredients, IngredientKeys } from 'common/types'
 
 const BASE_PRICE = 4
 
 type IngredientsMap = [IngredientKeys, number][]
 
-export const getTotalPrice = (ingredients:Ingredients, prices:Ingredients): number => {
-	const ingredientsMap = Object.entries(ingredients) as IngredientsMap;
+export const getTotalPrice = (
+	ingredients: Ingredients,
+	prices: Ingredients
+): number => {
+	const ingredientsMap = Object.entries(ingredients) as IngredientsMap
 	return ingredientsMap.reduce((acc, [key, value]) => {
 		acc += prices[key] * value
 		return acc
